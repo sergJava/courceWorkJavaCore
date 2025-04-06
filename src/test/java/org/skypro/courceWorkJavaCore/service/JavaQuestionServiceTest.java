90d45f81d5b4ffb27d04f00f544b62d059f57757
sergf <sergfibih@ya.ru>
2025-04-06 18:29:09 +1000

    курсовая повышенная сложность с тестами - готова

--- src/test/java/org/skypro/courceWorkJavaCore/service/JavaQuestionServiceTest.java
+++ src/test/java/org/skypro/courceWorkJavaCore/service/JavaQuestionServiceTest.java
@@ -8,10 +8,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.skypro.courceWorkJavaCore.model.Question;
+import org.skypro.courceWorkJavaCore.repository.JavaQuestionRepository;
 import org.skypro.courceWorkJavaCore.service.impl.JavaQuestionService;
 
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,48 +23,44 @@ public class JavaQuestionServiceTest {
     @Mock
     private Random random;
 
+    @Mock
+    private JavaQuestionRepository repository;
+
     @InjectMocks
     JavaQuestionService javaQuestionService;
 
+    Question question1 = new Question("q1", "a1");
+    Question question2 = new Question("q2", "a2");
+    Question question3 = new Question("q3", "a3");
+
     @Test()
-    void add_and_getAll() {
-        Question question1 = new Question("q1", "a1");
-        Question question2 = new Question("q2", "a2");
+    void addTest() {
+        Question result = javaQuestionService.add(question1);
 
-        javaQuestionService.add(question1);
-        javaQuestionService.add(question2);
-
-        Assertions.assertEquals(2, javaQuestionService.getAll().size());
-        Assertions.assertTrue(javaQuestionService.getAll().contains(question1));
-        Assertions.assertTrue(javaQuestionService.getAll().contains(question2));
-
-    }
-
-    @Test
-    void add_negativeResult() {
-        Question question1 = new Question("q1", "a1");
-        javaQuestionService.add(question1);
-
-        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class
-                , () -> javaQuestionService.add(question1));
-        Assertions.assertEquals("такой вопрос уже существует", exception.getMessage());
+        Mockito.verify(repository, Mockito.times(1)).add(question1);
+        Assertions.assertEquals(question1, result);
     }
 
     @Test
     void removeTest() {
-        Question question = javaQuestionService.add(new Question("q1", "a1"));
-        javaQuestionService.remove(question);
+        Mockito.when(repository.getAll()).thenReturn(Set.of(question1, question2));
 
-        Assertions.assertEquals(0, javaQuestionService.getAll().size());
-        Assertions.assertTrue(!javaQuestionService.getAll().contains(question));
+        Question result = javaQuestionService.remove(question1);
+
+        Mockito.verify(repository, Mockito.times(1)).remove(question1);
+        Assertions.assertEquals(question1, result);
+    }
+
+    @Test
+    void getAllTest(){
+        javaQuestionService.getAll();
+        Mockito.verify(repository, Mockito.times(1)).getAll();
     }
 
     @Test
     void getRandomQuestionTest() {
-        Question question1 = javaQuestionService.add(new Question("q1", "a1"));
-        Question question2 = javaQuestionService.add(new Question("q2", "a2"));
-        Question question3 = javaQuestionService.add(new Question("q3", "a3"));
-
+        List<Question> mockList = List.of(question1, question2, question3);
+        Mockito.when(repository.getAll()).thenReturn(mockList);
         Mockito.when(random.nextInt(anyInt())).thenReturn(2);
 
         Assertions.assertEquals(question3, javaQuestionService.getRandomQuestion());
